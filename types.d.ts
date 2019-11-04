d3509062a326b2d97a477fc9b0fa3b72eb38f9dc
Cameron Tacklind <cameron@tacklind.com>
2019-11-04 03:32:03 -0800

    Add docs to types

--- types.d.ts
+++ types.d.ts
@@ -1,2 +1,12 @@
+/**
+ * Sets the affinity of the main Node JavaScript thread
+ * @returns The bit mask, -1 on failure
+ */
 export declare function getAffinity(): number;
+
+/**
+ * Sets the affinity of the main Node JavaScript thread
+ * @returns The new mask, -1 on failure
+ * @param mask {Number} bit mask of which cores to run on
+ */
 export declare function setAffinity(mask: number): number;
