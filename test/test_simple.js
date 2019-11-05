a6e6b4d3dc6796a62e1be1542e9dcb423830e036
Cameron Tacklind <cameron@tacklind.com>
2019-11-04 17:36:06 -0800

    Add Prettier formatter for JavaScript

--- test/test_simple.js
+++ test/test_simple.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
-const assert = require("assert");
+const assert = require('assert');
 
-const nc = require("../");
+const nc = require('../');
 
 const affinity = nc.getAffinity();
 
@@ -10,6 +10,6 @@ const mask = affinity > 1 ? 2 : 1;
 
 const newAffinity = nc.setAffinity(mask);
 
-assert.equal(newAffinity, mask, "setAffinity failed to match");
+assert.equal(newAffinity, mask, 'setAffinity failed to match');
 
-assert.equal(nc.getAffinity(), mask, "getAffinity failed to match");
+assert.equal(nc.getAffinity(), mask, 'getAffinity failed to match');
