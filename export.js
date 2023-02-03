a4d0faab96a72399246e4b2228473187ac5b0e79
Saquib <saquibkhan@github.com>
2023-02-02 20:03:43 -0500

    Update export.js

--- export.js
+++ export.js
@@ -8,7 +8,9 @@ const repoName = "nodeaffinity";
 const repoOwner = "saquibkhan";
 axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}/issues`, { headers })
  .then(response => {
+ console.log("************** response data *******************");
   console.log(response.data);
   const workbook = XLSX.utils.json_to_sheet(response.data);
+   console.log("************** workbook *******************");
   console.log(workbook);
 XLSX.writeFile(XLSX.utils.book_new([workbook]), "issues.xlsx");});
